8f28ec222e3f9e7fc702ad82fe96069ac0fd1861
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-05-10 21:50:54 -0500

    backup current dotfiles

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,17 +6,25 @@
 
 ########## Variables
 
-dir=~/dotfiles   # dotfiles directory
-files="vimrc"    # list of files/folders to symlink in homedir
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="vimrc"                 # list of files/folders to symlink in homedir
 
 ##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
 
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-  echo "Creating symlink to $file in home directory."
-  ln -s $dir/$file ~/.$file
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
 done
